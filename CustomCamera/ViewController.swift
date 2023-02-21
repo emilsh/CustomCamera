1ba9e69cce6a77088a1174d5a05503aac18e8925
Emil Shafigin <emilshafigin@Emils-MacBook-Pro.local>
2023-02-21 10:34:13 +0600

    changing view color

--- CustomCamera/ViewController.swift
+++ CustomCamera/ViewController.swift
@@ -58,7 +58,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemGray4
+        view.backgroundColor = .systemGray5
         setupConstraints()
     }
     
